4b27f2d6eb6120d72fd11f0a3b9cc6a8e3a462a4
Ezequias Rocha <ezequias.rocha@ccc.ufcg.edu.br>
2021-01-08 20:45:56 -0300

    category API

--- config/routes.js
+++ config/routes.js
@@ -6,4 +6,16 @@ module.exports = app => {
   app.route('/users/:id')
     .put(app.api.user.save)
     .get(app.api.user.getById)
+
+  app.route('/categories')
+    .get(app.api.category.get)
+    .post(app.api.category.save)
+  
+  app.route('/categories/tree')
+    .get(app.api.category.getTree)
+    
+  app. route('/categories/:id')
+    .get(app.api.category.getById)
+    .put(app.api.category.save)
+    .delete(app.api.category.remove)
 }
