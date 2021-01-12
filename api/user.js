744f36ed66d3bd293b7dd6690cd025bcd7f03518
Ezequias Rocha <ezequias.rocha@ccc.ufcg.edu.br>
2021-01-12 13:38:47 -0300

    user soft delete and fixing bug

--- api/user.js
+++ api/user.js
@@ -35,6 +35,7 @@ module.exports = app => {
       app.db('users')
         .update(user)
         .where({ id: user.id })
+        .whereNull('deletedAt')
         .then(_ => res.status(204).send())
         .catch(err => res.status(500).send(err))
     } else {
@@ -48,15 +49,17 @@ module.exports = app => {
   const get = (req, res) => {
     app.db('users')
       .select('id', 'name', 'email', 'admin')
+      .whereNull('deletedAt')
       .then(users => res.json(users))
       .catch(err => res.status(500).send(err))
   }
 
   const getById = (req, res) => {
-    const userId = req.params.id
     app.db('users')
       .select('id', 'name', 'email', 'admin')
-      .where({ id: userId }).first()
+      .where({ id: req.params.id })
+      .whereNull('deletedAt')
+      .first()
       .then(user => res.json(user))
       .catch(err => res.status(500).send(err))
   }
@@ -64,11 +67,11 @@ module.exports = app => {
   const remove = async (req, res) => {
     try {
       const articles = await app.db('articles')
-        .where({ id: req.params.id })
+        .where({ userId: req.params.id })
       notExistsOrError(articles, 'Usuário possui artigos.')
 
       const rowsUpdated = await app.db('users')
-        .update({ deletedAt: new Date() })
+        .update({deletedAt: new Date()})
         .where({ id: req.params.id })
       existsOrError(rowsUpdated, 'Usuário não foi encontrado.')
 
--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,5 @@
 module.exports = middleware => {
-  return (req, res) => {
+  return (req, res, next) => {
     if (req.user.admin) {
       middleware(req, res, next)
     } else {
