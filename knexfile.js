cad8726a1bdef78be2c25e6d09eb6945501c4774
Ezequias Rocha <ezequias.rocha@ccc.ufcg.edu.br>
2021-01-12 14:00:40 -0300

    fix

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,8 @@
+const { db } = require('./.env')
+
 module.exports = {
   client: 'postgresql',
-  connection: {
-    database: 'knowledge',
-    user:     'postgres',
-    password: 'roch@bd1'
-  },
+  connection: db,
   pool: {
     min: 2,
     max: 10
